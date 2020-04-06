2f29c363b823fab7e0d14df633cca7f1e90ae358
pond18143 <58029432+pond18143@users.noreply.github.com>
2020-04-06 21:46:57 +0700

    covid

--- index.js
+++ index.js
@@ -166,13 +166,13 @@ function handleMessageText(event) {
     ({method:`GET`,
       uri:`covid19.th-stat.com/api/open/today`,
       json: true}).then((response) => {
-    message = `Confirmed: ${response.Confirmed}\nRecovered: ${response.Recovered}\nHospitalized: ${response.Hospitalized}\nDeaths: ${response.Deaths}\nNewConfirmed: ${response.NewConfirmed}\nNewRecovered: ${response.NewRecovered}\nNewHospitalized: ${response.NewHospitalized}\nNewNewDeaths: ${response.NewDeaths}\nUpdateDate: ${response.UpdateDate}`;})
+    message = `Confirmed: ${response.Confirmed}\nRecovered: ${response.Recovered}\nHospitalized: ${response.Hospitalized}\nDeaths: ${response.Deaths}\nNewConfirmed: ${response.NewConfirmed}\nNewRecovered: ${response.NewRecovered}\nNewHospitalized: ${response.NewHospitalized}\nNewNewDeaths: ${response.NewDeaths}\nUpdateDate: ${response.UpdateDate}`;
 
     msg ={
       'type':'text',
       'text':message
     }
-  
+  })
   
     
 
